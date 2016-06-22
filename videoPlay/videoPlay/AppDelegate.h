bc8ffe26260d58e18594b88bfb9a0cb2e7dd5905
lwg123 <656442711@qq.com>
2016-06-22 15:19:51 +0800

    update

---
+++ videoPlay/videoPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  videoPlay
+//
+//  Created by weiguang on 16/6/22.
+//  Copyright © 2016年 weiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ videoPlay/videoPlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  videoPlay
+//
+//  Created by weiguang on 16/6/22.
+//  Copyright © 2016年 weiguang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"shishi");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
